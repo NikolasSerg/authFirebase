392ee635f899a65c13656a9c26d470a4ea59ef03
NikolasSerg <nikolasalon@gmail.com>
2022-07-04 18:22:02 +0300

    first commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export default function NavBar() {
+    const [activeBtn, setActiveBtn] = useState()
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Button onClick={selectPage} variant="outlined">
+                        Login
+                    </Button>
+                    <Button variant="outlined">
+                        Register
+                    </Button>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+}
